c6318acd6d8f18b365565e8e196a5973e77c53a8
Simon van der Weele <simon.vanderweele@casasoft.com.mt>
2021-07-25 17:56:38 +0200

    fix: added webcam support

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+
+function BarcodeReader() {
+  const [data, setData] = React.useState("Not Found");
+
+  return (
+    <>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err: any, result: any) => {
+          console.log(result);
+          console.error(err);
+          if (result) setData(result.text);
+          else setData("Not Found");
+        }}
+      />
+      <p>{data}</p>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header"></header> */}
+      <BarcodeReader />
     </div>
   );
 }
